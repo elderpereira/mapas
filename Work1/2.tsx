80130e346ba5cb53299dbb8e3029665516c7eec3
elderpereira <minieldir@gmail.com>
2026-08-27 20:21:11 -0300

    teste

---
+++ Work1/2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="2" tilewidth="32" tileheight="32" tilecount="1032" columns="43">
+ <image source="2.png" width="1376" height="768"/>
+</tileset>
---
+++ Work1/img.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="img" tilewidth="32" tileheight="32" tilecount="1032" columns="43">
+ <image source="img.jpeg" width="1376" height="768"/>
+</tileset>
